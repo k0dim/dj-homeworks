8a319bbf1776bfa340099a779610d8921085d095
Konnov Dmitriy <yourgitemail@example.com>
2022-11-28 13:20:28 +0400

    pagination

--- 1.2-requests-templates/README.md
+++ 1.2-requests-templates/README.md
@@ -2,4 +2,24 @@
 
 Доброго времени суток!
 Предоставляю выполненные задания.
+
+*recipes: [recipes](1.2-requests-templates/pagination)
+*pagination: [pagination](1.2-requests-templates/recipes)
+
 Буду ожидать обратной связи!
+
+## Документация по проекту
+
+Для запуска проекта необходимо:
+
+Установить зависимости:
+
+```bash
+pip install -r requirements.txt
+```
+
+Выполнить команду:
+
+```bash
+python manage.py runserver
+```
--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
+from django.core.paginator import Paginator
+import csv
+from pagination.settings import BUS_STATION_CSV
 
 def index(request):
+
     return redirect(reverse('bus_stations'))
 
 
@@ -10,8 +14,17 @@ def bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
 
+    with open(BUS_STATION_CSV, 'r', encoding='UTF-8') as filecsv:
+        stations = [line_dir for line_dir in csv.DictReader(filecsv)]
+
+    CONTENT = stations
+    paginator = Paginator(CONTENT, 10)
+    page_number = int(request.GET.get("page", 1))
+    page = paginator.get_page(page_number)
+
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
-    }
+            'bus_stations': page.object_list,
+            'page': page,
+    }   
+
     return render(request, 'stations/index.html', context)
